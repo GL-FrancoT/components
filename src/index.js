f5e57e6e94f70956ea7e41638dc161e425de93bb
Franco Traverso <franco.traverso@globallogic.com>
2022-07-24 19:23:46 -0300

    Pass avatar and time as props

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM"/>
+      <CommentDetail author="Alex" timeAgo="Today at 2:00AM"/>
+      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM"/>
     </div>
   );
 };
